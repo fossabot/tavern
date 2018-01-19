1ff33d404dd9de7a9091cd402482a0a64e0ed082
Wésley Queiroz <wesley.coder@gmail.com>
2018-01-18 23:28:32 -0200

    Jest crashing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'nervjs'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import ActionHome from 'material-ui/svg-icons/action/home'
-// import c from 'classnames'
+import c from 'classnames'
+
 import s from './App.scss'
 
 const App = () => (
-  <div className={s.App}>
-    <header className={s.header}>
-      <ActionHome />
-      <h1 className={s.title}>Welcome to React</h1>
-    </header>
-    <p className={s.intro}>
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
+  <MuiThemeProvider>
+    <div className={c(s.App)}>
+      <header className={c(s.header)}>
+        <ActionHome />
+        <h1 className={c(s.title)}>Welcome to React</h1>
+      </header>
+      <p className={c(s.intro)}>
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  </MuiThemeProvider>
 )
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React, { render } from 'nervjs'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 
-const Render = () => <MuiThemeProvider>
-  <App />
-</MuiThemeProvider>
-
-render(<Render />, document.getElementById('root'))
+render(<App />, document.getElementById('root'))
 registerServiceWorker()
