3485ab7518c3fd205d6179ac3170366e1605f554
Wésley Queiroz <wesley.coder@gmail.com>
2018-03-07 12:53:55 -0300

    Page tests

--- __tests__/render.js
+++ __tests__/render.js
@@ -2,11 +2,18 @@ import React from 'react'
 import { mount } from 'enzyme'
 
 import App from '../pages'
+import About from '../pages/about'
 
-describe('With Enzyme', () => {
-  it('App shows "Home"', async () => {
+describe('Rendering should work', () => {
+  it('App page title is "Home"', async () => {
     const app = await mount(<App />)
 
     expect(app.find('h2').text()).toEqual('Home')
+  })
+
+  it('About page title is "About"', async () => {
+    const app = await mount(<About />)
+
+    expect(app.find('h2').text()).toEqual('About')
   })
 })
